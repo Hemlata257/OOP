ff3a0d792aead8074592fcacd3440a67bd0799d7
Hemlata257 <68540550+Hemlata257@users.noreply.github.com>
2020-07-26 19:47:19 +0530

    Add files via upload

---
+++ Assignment-1/greatest.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,max;
+cout<<"enter three numbers";
+cin>>a>>b>>c;
+max=a;
+if(max<b)
+{
+max=b;
+}
+if(max<c){
+
+max=c;
+}
+cout<<"the greatest number is"<<max;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
